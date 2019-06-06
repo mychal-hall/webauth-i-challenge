4c0b86c2e63cbdda017ccc562eb4267323c0ab43
Mychal <hallmychal@gmail.com>
2019-06-05 22:11:35 -0500

    MVP!

--- users/user-model.js
+++ users/user-model.js
@@ -2,12 +2,23 @@ const db = require("../data/dbConfig.js");
 
 module.exports = {
   insert,
-//   login,
-  get
+  get,
+  getBy,
+  getById
 };
 
 function get() {
   return db("users").select("id", "username", "password");
+}
+
+function getBy(filter) {
+  return db("users").where(filter);
+}
+
+function getById() {
+  return db("users")
+    .where("users.id", id)
+    .first();
 }
 
 async function insert(user) {
