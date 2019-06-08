2c2b9c9a140cc3819d4a12457e2c50818d4a7cf5
Mychal <hallmychal@gmail.com>
2019-06-08 11:56:43 -0500

    What what

--- server.js
+++ server.js
@@ -2,14 +2,33 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const session = require("express-session");
-const SessionStore = require("connect-session-knex")
-
+const SessionStore = require("connect-session-knex");
 
 // Routers go here
 const userRouter = require("./users/user-router.js");
 
 const server = express();
 
+const sessionConfig = {
+  name: "batman",
+  secret: "and robin",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    secure: false,
+    httpOnly: true
+  },
+  store: new SessionStore({
+    knex: require("./data/dbConfig.js"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 60 * 60 * 1000
+  })
+};
+
+server.use(session(sessionConfig));
 server.use(express.json());
 server.use(helmet());
 server.use(morgan("tiny"));
